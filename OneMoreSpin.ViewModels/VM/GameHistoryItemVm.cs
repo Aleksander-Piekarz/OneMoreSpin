28a801f2c446e2feee4bc9b9cc0b0c266f831a1d
Jakub Grzybowski <dfvfd>
2025-11-17 23:13:21 +0100

    fixed userscore.cs

--- OneMoreSpin.ViewModels/VM/GameHistoryItemVm.cs
+++ OneMoreSpin.ViewModels/VM/GameHistoryItemVm.cs
@@ -3,7 +3,7 @@ namespace OneMoreSpin.ViewModels.VM
     public class GameHistoryItemVm
     {
         public string GameName { get; set; } = string.Empty;
-        public decimal Score { get; set; }
+        public string Score { get; set; }
         public DateTime PlayedAt { get; set; }
     }
 }
